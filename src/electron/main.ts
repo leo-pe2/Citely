5beb89756923951a8e047b59651cb125f92bae3c
leope <l.penzenstadler@protonmail.com>
2025-09-05 16:20:19 +0200

    npm run improv

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -3,7 +3,14 @@ import path from 'path';
 
 type test = string;
 
+const isDev = process.env.NODE_ENV === 'development' || !app.isPackaged;
+
 app.on('ready', () => {
     const mainWindow = new BrowserWindow({});
-    mainWindow.loadFile(path.join(app.getAppPath() + '/dist-react/index.html'));
+    if (isDev) {
+        mainWindow.loadURL('http://localhost:5123');
+    } else {
+        mainWindow.loadFile(path.join(app.getAppPath() + '/dist-react/index.html'));
+
+    }
 });
